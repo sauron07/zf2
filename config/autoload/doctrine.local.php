66ec109695d77b3ec2c2180f68483c8709e8744a
sauron07 <matyeyev.sasha@gmail.com>
2014-09-20 09:41:31 +0300

    Edit gitignore file and add doctrine config file

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matveev
+ * Date: 9/11/14
+ * Time: 11:53 AM
+ */
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => '',
+                    'dbname'   => 'zf2',
+                )
+            )
+        )
+    )
+);
